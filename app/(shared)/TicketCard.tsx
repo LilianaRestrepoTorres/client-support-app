1e3b59cdd7f79c43b7d3678ec19994547d68db87
lilianarestrepotorres <lilianarestrepot@gmail.com>
2023-05-24 16:38:03 -0500

    Adding api, tickets view and tickets cards.

---
+++ app/(shared)/TicketCard.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+
+interface Props {
+  bot_id: string;
+  description_prompt: string;
+  problem_location: string;
+  problem_type: string;
+  status: string;
+  summary: string;
+  ticket_id: string;
+}
+
+const TicketCard: React.FC<Props> = (props) => {
+  return (
+    <div className="flex flex-col max-w-sm p-6 m-4 bg-white rounded-lg shadow-md">
+      <div className="flex justify-between pb-2 mb-2 border-b-2">
+        <div className="text-lg font-semibold text-gray-900">
+          Ticket ID: {props.ticket_id}
+        </div>
+        <div className="text-xs text-gray-500 ">Bot ID: {props.bot_id}</div>
+      </div>
+      <div className="mb-2">
+        <div className="text-sm font-medium text-gray-700">
+          Status: {props.status}
+        </div>
+        <div className="text-sm text-gray-500">
+          Problem Location: {props.problem_location}
+        </div>
+      </div>
+      <div className="mb-2">
+        <div className="text-sm font-medium text-gray-700">
+          Problem Type: {props.problem_type}
+        </div>
+        <div className="text-xs text-gray-500">Summary: {props.summary}</div>
+      </div>
+    </div>
+  );
+};
+
+export default TicketCard;
---
+++ app/tickets/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import api from "../api/api";
+import TicketCard from "../(shared)/TicketCard";
+
+const Tickets: React.FC = () => {
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      try {
+        const response = await api.get("/tickets");
+        setTickets(response.data);
+      } catch (error) {
+        console.error("Error fetching tickets", error);
+      }
+    };
+
+    fetchTickets();
+  }, []);
+
+  return (
+    <main className="flex flex-col items-start justify-start min-h-screen p-8 bg-gray-100">
+      <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {tickets.map((ticket: any) => (
+          <TicketCard key={ticket.ticket_id} {...ticket} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Tickets;
